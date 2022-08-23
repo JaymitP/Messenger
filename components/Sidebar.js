fb1d59629b5dd2be6d5a40e61fe381a794e856a6
JaymitP <18jaymit@gmail.com>
2022-08-23 21:32:56 +0100

    Added search & add chat button

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,40 +1,65 @@
 import Contact from "./Contact";
-import { HiPencilAlt, HiOutlineChat, HiSearch } from "react-icons/hi";
+import { HiPencilAlt, HiPlus, HiSearch } from "react-icons/hi";
 import { TiPin } from "react-icons/ti";
 import AddChat from "./AddChat";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useState } from "react";
 
 const Sidebar = ({ chats, users, setCurrentChat }) => {
   const [currentUser] = useAuthState(auth);
+  const [showAddChat, setShowAddChat] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
   return (
     <div className="w-[20vw] bg-secondary-dark border-r-neutral-700 border-r-2">
       <div className="sidebar_title p-6 text-3xl flex justify-between items-center">
         <h1 className="font-semibold">Messages</h1>
-        <HiPencilAlt className="fill-contrasting-dark text-2xl" />
+        <HiPencilAlt
+          className="fill-contrasting-dark text-2xl cursor-pointer"
+          onClick={() => setShowAddChat(!showAddChat)}
+        />
       </div>
 
       <div className="p-6">
-        <div className="flex items-center bg-tertiary-dark text-m rounded-md">
-          <AddChat currentUser={currentUser} />
-        </div>
+        {showAddChat ? (
+          <div className="flex items-center bg-tertiary-dark text-m rounded-md mb-2 px-1">
+            <AddChat currentUser={currentUser} />
+            {/* <HiPlus className="text-secondary-font-dark text-xl mr-2" /> */}
+          </div>
+        ) : null}
+
         <div className="flex items-center bg-tertiary-dark text-m rounded-md">
           <HiSearch className="m-2 fill-[#8294a6]" />
-          <input className="input-small" type="text" placeholder="Search" />
+          <input
+            className="input-small pr-2"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search"
+          />
         </div>
       </div>
 
       <div className="">
-        <div className="flex items-center gap-2 text-sm text-small px-6">
+        {/* <div className="flex items-center gap-2 text-sm text-small px-6">
           <TiPin />
           <p>PINNED</p>
+        </div> */}
+        <div className="flex items-center gap-2 text-sm text-small px-6">
+          <TiPin />
+          <p>ALL MESSAGES</p>
         </div>
+
         {chats.map((chat) => {
           const otherUser = chat.users.find((user) => user !== currentUser.uid);
           const contactData = users?.find((user) => user.id === otherUser);
           if (!contactData) return null;
-
+          if (
+            searchQuery &&
+            !contactData.name.toLowerCase().includes(searchQuery.toLowerCase())
+          )
+            return null;
           return (
             <Contact
               key={chat.id}
@@ -47,18 +72,6 @@ const Sidebar = ({ chats, users, setCurrentChat }) => {
             />
           );
         })}
-
-        <div className="flex items-center gap-2 text-sm text-small mt-8 px-6">
-          <TiPin />
-          <p>ALL MESSAGES</p>
-        </div>
-        <Contact
-          photoURL={undefined}
-          name="John Doe"
-          message="Lorem Ipsum..."
-          lastSeen="4:30pm"
-          notification="1"
-        />
       </div>
     </div>
   );
